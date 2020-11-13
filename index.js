fa95b864cdde3cfd284e517c60dd726cf24485cc
Meher Supreeth <mehersupreeth@gmail.com>
2020-11-13 06:24:24 +0530

    initial commit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ let port = process.env.PORT || 3000
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", ["https://music-angular.herokuapp.com"]); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", ["http://localhost:4200"]); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
